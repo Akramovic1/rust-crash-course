016a42e9e94750d62b856a638f8d16cec90f9281
Ahmed Akram <eng.akramovic@gmail.com>
2025-07-14 01:44:27 +0300

    Replaces HashMap::from with manual insertion
    Improves flexibility and clarity by manually inserting
    key-value pairs into the map, making initialization more
    explicit and adaptable to future changes.

--- topics/hash_map/exercises/src/lib.rs
+++ topics/hash_map/exercises/src/lib.rs
@@ -1,5 +1,8 @@
 use std::collections::HashMap;
 
 pub fn init(address: String, amount: u32) -> HashMap<String, u32> {
-    HashMap::from([(address, amount)])
+    //HashMap::from([(address, amount)])
+    let mut balances = HashMap::new();
+    balances.insert(address, amount);
+    balances
 }
